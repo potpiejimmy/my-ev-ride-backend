c14e5aad2cd39503a11318972e2142652effa477
potpiejimmy <t.liese@outlook.com>
2017-08-22 21:55:11 +0200

    initial db schema

---
+++ db/scripts/create.sql
@@ -0,0 +1,38 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+
+-- -----------------------------------------------------
+-- Table `asset`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `asset` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `make` VARCHAR(64) NOT NULL,
+  `model` VARCHAR(128) NOT NULL,
+  `image` VARCHAR(64) NOT NULL,
+  `lat` DECIMAL(10,8) NOT NULL,
+  `lon` DECIMAL(11,8) NOT NULL,
+  `value` INT NOT NULL,
+  `currency` CHAR(3) NOT NULL,
+  `sharecount` SMALLINT NOT NULL,
+  `infotext` VARCHAR(4096) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `user` (
+  `name` VARCHAR(64) NOT NULL,
+  `type` TINYINT NOT NULL DEFAULT 0,
+  `password` VARCHAR(32) NULL,
+  `pw_status` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`name`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
