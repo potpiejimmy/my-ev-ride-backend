9174e12fc7386270bd5a238593bd708873f0daac
potpiejimmy <t.liese@outlook.com>
2017-08-23 21:35:58 +0200

    added column user_name as ref integ

--- db/scripts/create.sql
+++ db/scripts/create.sql
@@ -4,10 +4,23 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 
 -- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `user` (
+  `name` VARCHAR(64) NOT NULL,
+  `type` TINYINT NOT NULL DEFAULT 0,
+  `password` VARCHAR(32) NULL,
+  `pw_status` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`name`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `asset`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `asset` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(64) NOT NULL,
   `make` VARCHAR(64) NOT NULL,
   `model` VARCHAR(128) NOT NULL,
   `image` VARCHAR(64) NOT NULL,
@@ -17,19 +30,13 @@ CREATE TABLE IF NOT EXISTS `asset` (
   `currency` CHAR(3) NOT NULL,
   `sharecount` SMALLINT NOT NULL,
   `infotext` VARCHAR(4096) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `user` (
-  `name` VARCHAR(64) NOT NULL,
-  `type` TINYINT NOT NULL DEFAULT 0,
-  `password` VARCHAR(32) NULL,
-  `pw_status` TINYINT NOT NULL DEFAULT 0,
-  PRIMARY KEY (`name`))
+  PRIMARY KEY (`id`),
+  INDEX `fk_asset_user_idx` (`user_name` ASC),
+  CONSTRAINT `fk_asset_user`
+    FOREIGN KEY (`user_name`)
+    REFERENCES `user` (`name`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
