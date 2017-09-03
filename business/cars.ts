20c4645bbd83a5d6be3f7e419f1b90c4dbf0052c
potpiejimmy <t.liese@outlook.com>
2017-09-03 19:41:33 +0200

    join user's shortened display_name for result table

--- business/cars.ts
+++ business/cars.ts
@@ -6,7 +6,10 @@ import * as utils from "../util/utils";
  * @return list of cars
  */
 export function getCars(lon: number, lat: number): Promise<any> {
-    return db.querySingle("SELECT * FROM asset ORDER BY (POW((lon-?),2) + POW((lat-?),2))",[lon,lat]);
+    return db.querySingle("SELECT asset.*,user.display_name FROM asset JOIN user ON asset.user_id=user.id ORDER BY (POW((lon-?),2) + POW((lat-?),2)) LIMIT 1000",[lon,lat]).then(res => {
+        res.forEach(e => e.display_name = shortenDisplayName(e.display_name));
+        return res;
+    });
 }
 
 export function getCar(id: number): Promise<any> {
@@ -32,4 +35,10 @@ export function deleteCar(user: any, id: number) {
         if (user.id !== car.user_id) throw "You are trying to delete a car that is not yours.";
         return db.querySingle("DELETE FROM asset WHERE id=?", [id]);
     })
+}
+
+function shortenDisplayName(e: string): string {
+    let spacePos = e.indexOf(" ");
+    if (spacePos > 0) return e.substr(0, spacePos);
+    return e;
 }
